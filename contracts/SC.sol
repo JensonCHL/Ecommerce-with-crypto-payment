0bf65ee375e0456fcdc1dfa07f38137da2cb0467
JensonCHL <j3n50n999@gmail.com>
2024-07-22 11:55:31 +0700

    day 2

--- contracts/SC.sol
+++ contracts/SC.sol
@@ -1,11 +1,79 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 contract SC {
-    
-    string public name;
+    address public owner;
 
-    constructor(){
-        name = "JoeKowi";
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
     }
+
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
+    mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
+
+    event List(string name, uint256 cost, uint256 quantity);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // list products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner {
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        items[_id] = item;
+
+        emit List(_name, _cost, _stock);
+    }
+
+    // buy products
+    function buy(uint256 _id) public payable {
+        // Fetch item
+        Item memory item = items[_id];
+        // Create an order
+        Order memory order = Order(block.timestamp, item);
+        // Save order to chain
+        orderCount[msg.sender]++;// order id
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        // substract stock
+        items[_id].stock--;
+
+        // emit event
+    }
+
+    // Withdraw funds
 }
--- test/test.js
+++ test/test.js
@@ -1,16 +1,107 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-const tokens = (n)=>{
-  return ethers.utils.parseUnits(n.toString(),'ether')
+const tokens = (n) => {
+  return ethers.parseUnits(n.toString(), 'ether')
 }
-
+const ID = 1
+const NAME = "Shoes"
+const CATEGORY = "Clothing"
+const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg"
+const COST = tokens(1)
+const RATING = 4
+const STOCK = 5
 describe('Joekowi', () => {
   let sc;
-  it('has a name', async () => {
+  let deployer, buyer;
+  beforeEach(async () => {
+    // Setup accounts
+    [deployer, buyer] = await ethers.getSigners()
+    // console.log(deployer.address,buyer.address)
     const Sc = await ethers.getContractFactory('SC')
     sc = await Sc.deploy()
-    const name = await sc.name();
-    expect(name).to.equal("JoeKowi")
+    // await sc.waitForDeployment();
+    SCAddress = await sc.getAddress();
+    // 
+  })
+
+  describe('Deployment', () => {
+    it('ensure owner', async () => {
+      expect(await sc.owner()).to.equal(deployer.address);
+    });
+
   });
-  
+  describe('Listing', () => {
+    let transaction
+
+    beforeEach(async () => {
+      transaction = await sc.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+      await transaction.wait()
+    })
+    it('returns item attributes', async () => {
+      const item = await sc.items(ID)
+      expect(item.id).to.equal(ID)
+    });
+    it('emit list event', () => {
+      expect(transaction).to.emit(sc, "List")
+    });
+
+
+  });
+
+  describe('Listing', () => {
+    let transaction
+
+    beforeEach(async () => {
+      transaction = await sc.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+      await transaction.wait()
+      transaction = await sc.connect(buyer).buy(ID, { value: COST })
+      await transaction.wait()
+
+    })
+
+
+    it('update contract balance', async () => {
+      if (!SCAddress) {
+        throw new Error('Contract address is not defined');
+      }
+      const result = await ethers.provider.getBalance(SCAddress)
+      expect(result).to.equal(COST)
+    })
+
+    it('update buyers order count',async () => {
+      const result = await sc.orderCount(buyer.address);
+      expect(result).to.equal(1)
+    });
+    it('Adds order', async() => {
+      const order = await sc.orders(buyer.address,1)
+      expect(order.time).to.be.greaterThan(0)
+      expect(order.item.name).to.equal(NAME)
+
+    });
+    
+    
+
+  });
+
+
+
+
+
 });
